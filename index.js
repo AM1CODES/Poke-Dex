a711bc080ef0e1ff80ebbd9439a6ddb4f1f8fbd9
nkzren <renan.nakazawa0@gmail.com>
2021-10-06 21:55:23 -0300

    add pokemon search

--- index.js
+++ index.js
@@ -34,6 +34,37 @@ document.querySelector('#compact-view').addEventListener('mousedown', () => {
   setNormalView(false);
 });
 
+// START POKEMON SEARCH
+const pokemons = []
+
+document.querySelectorAll('h5.card-title').forEach(e => {
+  pokemons.push({name: e.innerHTML})
+})
+
+document.querySelector('#search-button').addEventListener('mousedown', () => {
+  const index = lunr(function () {
+    this.ref('name')
+    this.field('name')
+
+    pokemons.forEach(function (doc) {
+      this.add(doc)
+    }, this)
+  });
+
+  const search = document.querySelector('#search-bar').value;
+  const results = index.search(search).map(e => e.ref);
+  console.log(results)
+  document.querySelectorAll('#gallery > .container > .row > div').forEach(e => {
+    const pokemonName = e.querySelector('.card-title').innerHTML
+    if (!results.includes(pokemonName)) {
+      e.style.display = 'none';
+    } else {
+      e.style.display = 'block';
+    }
+  })
+});
+// END POKEMON SEARCH
+
 const fixBrokenImages = () => {
     const fallbackURL = 'images/fallback_image.png'
     const img = document.getElementsByTagName('img');
