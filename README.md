a98963c5b7e0399e89adc8ecdeb1a56d72d0f02c
RichardHpa <richard.hpa@hotmail.com>
2021-10-14 21:50:00 +1300

    ability to add pokemon via a js file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![Pokémon](https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png)
 
-There's no doubt that most of our childhood was spent watching Pokémon and playing games like Pokémon Fire-Red, Ruby, Emerald, etc. From Kanto to Hoenn to Sinnoh, we have seen it all and experienced it all. When Hacktoberfest 2020 came up, I was one of the people who would participate for the very first time, so I decided to create this repository that's actually a very basic website that lets users view and add their favourite Pokémon.
+There's no doubt that most of our childhood was spent watching Pokémon and playing games like Pokémon Fire-Red, Ruby, Emerald, etc. From Kanto to Hoenn to Sinnoh, we have seen it all and experienced it all. With Hacktoberfest 2020 coming up and being one of the people who would participate for the very first time, I decided to create this repository that's actually a very basic website that lets users view and add their favourite Pokémon.
 
 # Contents of the website
 
@@ -16,6 +16,7 @@ There's no doubt that most of our childhood was spent watching Pokémon and play
 There is always something to contribute to a project whether you are a novice or a veteran. In this project, you can add your favourite Pokémon as a card to the website! Or maybe you didn't like the line that you are reading right now and want to change that. Sure! Why not?
 
 # How can I contribute?
+
 Kindly refer to [CONTRIBUTING.md](https://github.com/AM1CODES/Poke-Dex/blob/master/CONTRIBUTING.md) file to learn how to contribute!
 
 And that's it!
@@ -27,23 +28,39 @@ Not everyone is aware of every tool present in the world no matter how easy or c
 
 ```html
 <div class="col-lg-4 mb-4">
-  <div class="card">
-    <img class="card-img-top" src="[Link to Pokémon image]" alt="" />
-    <div class="card-body">
-      <h5 class="card-title">[Name of your favourite Pokémon]</h5>
-      <h5 class="card-subtitle">#[ID of your favourite Pokémon]</h5>
-      <p class="card-text">[Small Description about your Pokémon]</p>
-      <a
-        href="[Add the link to your GitHub Profile]"
-        class="btn btn-outline-danger btn-sm"
-        >Contributed by - [Add your GitHub Username/profile name]</a
-      >
-    </div>
-  </div>
+	<div class="card">
+		<img class="card-img-top" src="[Link to Pokémon image]" alt="" />
+		<div class="card-body">
+			<h5 class="card-title">[Name of your favourite Pokémon]</h5>
+			<p class="card-text">[Small Description about your Pokémon]</p>
+			<a
+				href="[Add the link to your GitHub Profile]"
+				class="btn btn-outline-danger btn-sm"
+				>Contributed by - [Add your GitHub Username/profile name]</a
+			>
+		</div>
+	</div>
 </div>
 ```
 
 Copy this code and paste it in the index.html file to make your changes. Test it on your own system and then create a pull request.
+
+Or an even easier way is to just add an object to the `pokemon.js` file with
+
+```js
+	{
+		pokemonName: [Name of your favourite Pokémon],
+		pokemonDescription: [Small Description about your Pokémon],
+		pokemonImage: [Link to Pokémon image],
+		contributedByName: [Add your Github Username],
+		contributedByUrl: [Add a link to your Github profile],
+		// these are only needed if you have improved a card
+		improvedByName: [Add your Github Username],
+		improvedByUrl: [Add a link to your Github profile],
+	},
+```
+
+If all of the pokemon get added to this file, then we can extend this project to include functionality like search and filtering.
 
 # Congratulations!
 
--- index.js
+++ index.js
@@ -1,155 +1,87 @@
 const setNormalView = (isNormalView) => {
-  if (isNormalView) {
-    document.querySelector("#normal-view").classList.add("selected");
-    document.querySelector("#compact-view").classList.remove("selected");
+	if (isNormalView) {
+		document.querySelector('#normal-view').classList.add('selected')
+		document.querySelector('#compact-view').classList.remove('selected')
 
-    document.querySelectorAll(".card-body").forEach((cardBody) => {
-      cardBody.classList.remove("hidden");
-    });
+		document.querySelectorAll('.card-body').forEach((cardBody) => {
+			cardBody.classList.remove('hidden')
+		})
 
-    document.querySelectorAll(".row>div").forEach((ele) => {
-      ele.classList.replace("col-lg-2", "col-lg-4");
-    });
-  } else {
-    document.querySelector("#compact-view").classList.add("selected");
-    document.querySelector("#normal-view").classList.remove("selected");
+		document.querySelectorAll('.row>div').forEach((ele) => {
+			ele.classList.replace('col-lg-2', 'col-lg-4')
+		})
+	} else {
+		document.querySelector('#compact-view').classList.add('selected')
+		document.querySelector('#normal-view').classList.remove('selected')
 
-    document.querySelectorAll(".card-body").forEach((cardBody) => {
-      cardBody.classList.add("hidden");
-    });
+		document.querySelectorAll('.card-body').forEach((cardBody) => {
+			cardBody.classList.add('hidden')
+		})
 
-    document.querySelectorAll(".row>div").forEach((ele) => {
-      ele.classList.replace("col-lg-4", "col-lg-2");
-    });
-  }
-};
-
-setNormalView(true); // By default normal view is enabled.
-
-document.querySelector("#normal-view").addEventListener("mousedown", () => {
-  setNormalView(true);
-});
-
-document.querySelector("#compact-view").addEventListener("mousedown", () => {
-  setNormalView(false);
-});
-
-const sortCards = byName => {
-  const galleryRow = document.querySelector('.row');
-  const cards = [...galleryRow.querySelectorAll(".col-lg-4")];
-  galleryRow.innerHTML = "";
-  if (byName) {
-    cards.sort((c1, c2) => {
-      return c1.querySelector(".card-title").textContent < c2.querySelector(".card-title").textContent ? -1 : 1;
-    })
-    console.log(cards)
-  } else {
-    cards.sort((c1, c2) => {
-      return parseInt(c1.querySelector(".card-subtitle").textContent.substr(1)) - parseInt(c2.querySelector(".card-subtitle").textContent.substr(1));
-    })
-    console.log(cards)
-  }
-
-  cards.forEach(c => {
-    galleryRow.appendChild(c);
-  })
+		document.querySelectorAll('.row>div').forEach((ele) => {
+			ele.classList.replace('col-lg-4', 'col-lg-2')
+		})
+	}
 }
 
-document.querySelector('#sort-number').addEventListener('click', () => {
-  sortCards(false);
-  document.querySelector('#sort-number').classList.add('active')
-  document.querySelector('#sort-name').classList.remove('active')
+setNormalView(true) // By default normal view is enabled.
+
+document.querySelector('#normal-view').addEventListener('mousedown', () => {
+	setNormalView(true)
 })
 
-document.querySelector('#sort-name').addEventListener('click', () => {
-  sortCards(true);
-  document.querySelector('#sort-name').classList.add('active')
-  document.querySelector('#sort-number').classList.remove('active')
+document.querySelector('#compact-view').addEventListener('mousedown', () => {
+	setNormalView(false)
 })
-
-// START POKEMON SEARCH
-const pokemons = [];
-
-document.querySelectorAll("h5.card-title").forEach((e) => {
-  pokemons.push({ name: e.innerHTML });
-});
-
-document.querySelector("#search-button").addEventListener("click", () => {
-  if(!document.querySelector("#search-bar").value) return alert("Please enter a Pokémon name");
-
-  const index = lunr(function () {
-    this.ref("name");
-    this.field("name");
-
-    pokemons.forEach(function (doc) {
-      this.add(doc);
-    }, this);
-  });
-
-  const search = document.querySelector("#search-bar").value;
-  const results = index.search(search).map((e) => e.ref);
-  console.log(results);
-  document
-    .querySelectorAll("#gallery > .container > .row > div")
-    .forEach((e) => {
-      const pokemonName = e.querySelector(".card-title").innerHTML;
-      if (!results.includes(pokemonName)) {
-        e.style.display = "none";
-      } else {
-        e.style.display = "block";
-      }
-    });
-});
-// END POKEMON SEARCH
 
 const fixBrokenImages = () => {
-  const fallbackURL = "images/fallback_image.png";
-  const img = document.getElementsByTagName("img");
-  for (let i = 0; i < img.length; i++) {
-    let t = img[i];
-    if (t.naturalWidth === 0) {
-      t.src = fallbackURL;
-    }
-  }
-};
-
-window.onload = fixBrokenImages;
-
-// START OF "TRIGGERING SEARCH BUTTON WHEN ENTER KEY IS CLICKED"
-$("#search-bar").keypress(function (event) {
-  if (event.keyCode === 13) {
-    $("#search-button").click();
-  }
-});
-// END OF "TRIGGERING SEARCH BUTTON WHEN ENTER KEY IS CLICKED"
-
-const toggle = document.querySelector("#toggle");
-toggle.addEventListener("click", modeSwitch);
-
-let isLight = true;
-
-function modeSwitch() {
-  isLight = !isLight;
-  let root = document.body;
-
-  isLight ? (toggle.innerText = "🌞") : (toggle.innerText = "🌚");
-
-  root.classList.toggle("lightMode");
+	const fallbackURL = 'images/fallback_image.png'
+	const img = document.getElementsByTagName('img')
+	for (let i = 0; i < img.length; i++) {
+		let t = img[i]
+		if (t.naturalWidth === 0) {
+			t.src = fallbackURL
+		}
+	}
 }
 
+$(document).ready(function () {
+	const allPokemon = window.pokemon
+	const template = $('#cardTemplate').html()
 
-$(window).scroll(function() {
-  var top_of_element = $("#gallery").offset().top;
-  var bottom_of_element = $("#gallery").offset().top + $("#gallery").outerHeight();
-  var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-  var top_of_screen = $(window).scrollTop();
-  
-  var r = document.querySelector(':root');
-  if ((bottom_of_screen > top_of_element+300) && (top_of_screen < bottom_of_element)){
-      r.style.setProperty('--arrow-display', 'block')
-    } else {
-      r.style.setProperty('--arrow-display', 'none')
+	allPokemon.forEach((pokemon) => {
+		const card = $(template).clone()
+		card
+			.find('img')
+			.attr('src', pokemon.pokemonImage)
+			.attr('alt', pokemon.pokemonName)
 
-  }
-});
+		card.find('.card-title').text(pokemon.pokemonName)
+		card.find('.card-text').text(pokemon.pokemonDescription)
+		card.find('a').text(`Contributed by - ${pokemon.contributedByName}`)
+		let button = card.find('a').clone()
+		if (pokemon.contributedByUrl) {
+			card.find('a').attr('href', pokemon.contributedByUrl)
+		} else {
+			card.find('a').replaceWith(function () {
+				return $('<p />', { html: $(this).html() })
+			})
+		}
+		if (pokemon.improvedByName) {
+			button
+				.attr('style', 'margin-top: 20px')
+				.text(`Improved by - ${pokemon.improvedByName}`)
+			if (pokemon.improvedByUrl) {
+				console.log('here')
+				button.attr('href', pokemon.contributedByUrl)
+			} else {
+				button = `<p>Improved by - ${pokemon.improvedByName}</p>`
+			}
+			card.find('.card-body').append(button)
+		}
 
+		card.appendTo('#pokemon-row')
+	})
+})
+
+window.onload = fixBrokenImages
